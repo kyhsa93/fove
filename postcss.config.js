f23dc89aca1ef893015ce89db59c396204d9958f
kyhsa93 <kyhsa93@naver.com>
2025-09-23 21:39:34 +0900

    refactor: update packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
+export default {
+  plugins: [tailwindcss(), autoprefixer()],
+}
