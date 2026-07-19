50627a444b262463ea6761c45ffb1dc920112777
Young hoon <younghoon@jihyeui-MacBookAir.local>
2026-07-19 17:08:52 +0900

    chore: github page 로 전환

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import './index.css'
 import { ViteReactSSG } from 'vite-react-ssg'
 import { routes } from './router'
 
-export const createRoot = ViteReactSSG({ routes })
+export const createRoot = ViteReactSSG({
+  routes,
+  basename: import.meta.env.BASE_URL,
+})
